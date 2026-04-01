5e46106ff9dab447819d12a79a95b716eecd02ac
tharunprasath45 <tharunprasath288@gmail.com>
2026-04-01 13:47:24 +0530

    JOBINSIGHTS

--- client/src/components/Jobinsights.jsx
+++ client/src/components/Jobinsights.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./jobinsights.css";
+import "./Jobinsights.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { renderheader, header } from "./Dashboardcontent";
